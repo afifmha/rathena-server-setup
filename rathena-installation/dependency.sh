0b3cd81e0a4f929b004c5f2f08873349b031bac3
unknown <afifmha@gmail.com>
2026-08-17 17:43:02 +0700

    fix(script): fix permission command for dependency

--- rathena-installation/dependency.sh
+++ rathena-installation/dependency.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 
-apt-get install -y git make libmariadb-dev \
+sudo apt install -y git make libmariadb-dev \
 libmariadb-dev-compat \
 gcc g++ zlib1g-dev \
 libpcre3-dev screen
