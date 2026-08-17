159ac3ded3d8c266bd57a7ea5ef6c79e1711047e
unknown <afifmha@gmail.com>
2026-08-17 17:18:39 +0700

    fix(script): fix dependency name

--- rathena-installation/dependency.sh
+++ rathena-installation/dependency.sh
@@ -3,8 +3,7 @@
 sudo apt update && sudo apt upgrade -y
 
 apt-get install -y git make libmariadb-dev \
-libmariadbclient-dev \
-libmariadbclient-dev-compat \
+libmariadb-dev-compat \
 gcc g++ zlib1g-dev \
 libpcre3-dev screen
 
